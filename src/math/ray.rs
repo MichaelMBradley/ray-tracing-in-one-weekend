6a1219e68dc1f17b2fd89f77b0779df028fcd788
Michael Bradley <michaelmbrad@gmail.com>
2023-06-16 00:04:17 -0400

    Add ray struct

---
+++ src/math/ray.rs
@@ -0,0 +1,28 @@
+use crate::math::vec3::Vec3;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Ray {
+    orig: Vec3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(orig: &Vec3, dir: &Vec3) -> Self {
+        Self {
+            orig: *orig,
+            dir: *dir,
+        }
+    }
+
+    pub fn origin(&self) -> Vec3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Vec3 {
+        self.orig + t * self.dir
+    }
+}
